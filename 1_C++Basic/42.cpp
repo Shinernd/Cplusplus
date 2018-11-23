c2574f3433f97ae777987d26b67ad146a70c16d6
Shinernd <vnflqkq@gmail.com>
2018-11-24 02:54:19 +0900

    restart

---
+++ 1_C++Basic/42.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "The answer to the Ultimate Question of Life, \n"
+		<< "the Universe, and Everything is: "
+		<< std::endl << 6 * 7 << std::endl;
+
+	system("pause");
+	return 0;
+}
---
+++ 1_C++Basic/GeneralStream.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+void write_something(std::ostream& os)
+{
+    os << "Hi stream, did you know that 3 * 3 = " << 3 * 3 << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    std::ofstream myfile("example.txt");
+    std::stringstream mysstream;
+
+    write_something(std::cout);
+    write_something(myfile);
+    write_something(mysstream);
+
+    std::cout << "mysstream is: " << mysstream.str(); //개행 포함
+}
