a960b2300ffd94f0b89c2f719f7d9668390b45d1
Shinernd <vnflqkq@gmail.com>
2018-11-27 17:29:18 +0900

    Pointers and Dynamic Memory

--- 0_Tutorial/PointersAsArguments.cpp
+++ 0_Tutorial/PointersAsArguments.cpp
@@ -11,6 +11,13 @@ void increment_all (int* start, int* stop)
     }
 }
 
+/*
+*p++   // same as *(p++): increment pointer, and dereference unincremented address
+*++p   // same as *(++p): increment pointer, and dereference incremented address
+++*p   // same as ++(*p): dereference pointer, and increment the value it points to
+(*p)++ // dereference pointer, and post-increment the value it points to
+*/
+
 void print_all (const int* start, const int* stop)
 {
     const int* current = start;
@@ -20,6 +27,15 @@ void print_all (const int* start, const int* stop)
         ++current; // increment pointer
     }
 }
+
+/*
+int x;
+      int *       p1 = &x;  // non-const pointer to non-const int
+const int *       p2 = &x;  // non-const pointer to const int
+int const *       p2 = &x;  // also non-const pointer to const int
+      int * const p3 = &x;  // const pointer to non-const int
+const int * const p4 = &x;  // const pointer to const int
+*/
 
 int main()
 {
